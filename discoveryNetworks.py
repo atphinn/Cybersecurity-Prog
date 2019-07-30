f1f26feef20d5b0580789037dacf522b977e4ddf
atphinn <atphinn@gmail.com>
2019-07-29 22:10:28 -0400

    added wigle functionality to disconveryNetworks.py

--- discoveryNetworks.py
+++ discoveryNetworks.py
@@ -1,3 +1,9 @@
+import mechanize
+import urllib
+import re 
+import urlparse 
+import os 
+import optparse
 from _winreg import *
 
 def val2addr(val):
@@ -8,9 +14,34 @@ def val2addr(val):
     addr = addr.strip(" ").replace("", ":")[0:17]
     return addr
 
-def printNets():
-    net = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion"+\
-          r"\NetworkList\Signatures\Unmanaged"
+def wiglePrint(username, password, netid):
+    browser = mechanize.Browser()
+    browser.open('http://wigle.net')
+    reqData = urllib.urlencode({'credentials_0':username, 'credential_1':password})
+
+    browser.open('https://wigle.net/gps/gps/main/login',reqData)
+    params = {}
+    params['netid'] = netid
+
+    reqParams = urllib.urlencode(params)
+    respURL = "http://wigle.net/gps/gps/main/confirmquery/"
+    resp = browser.open(respURL, reqParams).read()
+    mapLat = 'N/A'
+    mapLon = 'N/A'
+    rLat = re.findall(r'maplat=.*\&', resp)
+    
+    if rLat:
+        mapLat = rLat[0].split('&')[0].split('=')[1]
+
+    rLon = re.findall(r'maplon+.*\&', resp)
+
+    if rLon:
+        mapLon = rLon[0].split
+    print('[-]Lat:' + mapLat + ',Lon:' + mapLon) 
+
+def printNets(username, password):
+    net =\ 
+        "SOFTWARE\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\Unmanaged"
     
     key = OpenKey(HKEY_LOCAL_MACHINE, net)
 
@@ -26,11 +57,36 @@ def printNets():
             netName = str(name)
 
             print("[+]" + netName + "" + macAddr)
+            wiglePrint(username, password, macAddr)
             CloseKey(netKey)
         except:
             break
+
+
 def main():
-    printNets()
+    parser = \
+        optparse.OptionParser("usage%prog"+
+        "-u <wigle username> -p <wigle password>"
+        )
+    
+    parser.add_option('-u', dest='username', type='string',
+    help='specify wigle username')
+    parser.add_option('-p', dest='password', type='string',
+    help='specify wigle password')
+
+    (options, args) = parser.parse_args()
+
+    username = options.username
+    password = options.password
+
+    if username == None or password == None:
+        print(parser.usage)
+        exit(0)
+    else:
+        printNets(username, password)
+
+if __name__ == '__main__':
+    main()
 
 if __name__ == "__main__":
     main()
